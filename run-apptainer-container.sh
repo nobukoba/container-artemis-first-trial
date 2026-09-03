c1163f9a10bddffb56d11b03eb389e01dad4a1c7
nobukoba <kobayash@rcnp.osaka-u.ac.jp>
2026-09-03 11:42:00 +0900

    Use login shell for Apptainer helper

--- run-apptainer-container.sh
+++ run-apptainer-container.sh
@@ -6,7 +6,7 @@ WORK_DIR="${WORK_DIR:-$PWD/work}"
 
 mkdir -p "${WORK_DIR}"
 
-args=(shell --bind "${WORK_DIR}:/work")
+args=(exec --bind "${WORK_DIR}:/work")
 
 if [[ -n "${DISPLAY:-}" ]]; then
   args+=(--env "DISPLAY=${DISPLAY}")
@@ -15,4 +15,4 @@ if [[ -d /tmp/.X11-unix ]]; then
   args+=(--bind /tmp/.X11-unix:/tmp/.X11-unix)
 fi
 
-apptainer "${args[@]}" "${SIF}"
+apptainer "${args[@]}" "${SIF}" /bin/bash -l
